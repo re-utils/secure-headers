db54067220e94547c866ac357288b1f6e4e1d7f1
aquapi <aquaplmc@gmail.com>
2025-08-23 14:35:35 +0700

    Update

--- src/index.ts
+++ src/index.ts
@@ -9,37 +9,53 @@ export const reportEndpoints = (endpoints: Record<string, string>): string =>
 export interface Options {
   'content-security-policy'?: string;
   'content-security-policy-report-only'?: string;
+
   'strict-transport-security'?: string;
+
   'cross-origin-embedder-policy'?:
-    | 'unsafe-none'
-    | 'require-corp'
-    | 'credentialless';
-  'cross-origin-resource-policy'?: 'same-site' | 'same-origin' | 'cross-origin';
+  | 'unsafe-none'
+  | 'require-corp'
+  | 'credentialless';
+  'cross-origin-resource-policy'?:
+  | 'same-site'
+  | 'same-origin'
+  | 'cross-origin';
   'cross-origin-opener-policy'?:
-    | 'unsafe-none'
-    | 'same-origin-allow-popups'
-    | 'same-origin'
-    | 'noopener-allow-popups';
+  | 'unsafe-none'
+  | 'same-origin-allow-popups'
+  | 'same-origin'
+  | 'noopener-allow-popups';
+
   'referrer-policy'?:
-    | 'no-referrer'
-    | 'no-referrer-when-downgrade'
-    | 'origin'
-    | 'origin-when-cross-origin'
-    | 'same-origin'
-    | 'strict-origin'
-    | 'strict-origin-when-cross-origin'
-    | 'unsafe-url';
+  | 'no-referrer'
+  | 'no-referrer-when-downgrade'
+  | 'origin'
+  | 'origin-when-cross-origin'
+  | 'same-origin'
+  | 'strict-origin'
+  | 'strict-origin-when-cross-origin'
+  | 'unsafe-url';
+
   'origin-agent-cluster'?: '?0' | '?1';
+
+  'x-frame-options'?: 'DENY' | 'SAMEORIGIN';
+  'x-content-type-options'?: 'nosniff';
+  'x-permitted-cross-domain-policies'?: 'none' | 'master-only' | 'by-content-type' | 'by-ftp-filename' | 'all' | 'none-this-response';
 }
 
 export const defaults: Options = {
-  "content-security-policy": "default-src 'self'",
-  "cross-origin-embedder-policy": "require-corp",
-  "cross-origin-opener-policy": "same-origin",
-  "cross-origin-resource-policy": "same-origin",
-  "origin-agent-cluster": "?1",
-  "referrer-policy": "no-referrer",
-  "strict-transport-security": "max-age=15552000; includeSubDomains"
+  'content-security-policy': "default-src 'self'",
+  'cross-origin-embedder-policy': 'require-corp',
+  'cross-origin-opener-policy': 'same-origin',
+  'cross-origin-resource-policy': 'same-origin',
+  'origin-agent-cluster': '?1',
+  'referrer-policy': 'no-referrer',
+  'strict-transport-security': 'max-age=15552000; includeSubDomains',
+  'x-frame-options': 'SAMEORIGIN',
+  'x-content-type-options': 'nosniff',
+  'x-permitted-cross-domain-policies': 'none'
 };
 
-export const secureHeaders = (headers?: Options): Options => Object.assign({}, defaults, headers);
+export const secureHeaders = (headers?: Options): Options => ({
+  ...defaults, ...headers
+});
